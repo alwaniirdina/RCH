1d2d7861e616d58d49b2aa41eb300393117fee56
alwaniirdina <156833210+alwaniirdina@users.noreply.github.com>
2024-01-17 01:03:25 +0800

    Add files via upload

---
+++ style.js
@@ -0,0 +1,79 @@
+function openCity(cityName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+
+  // Show the specific tab content
+  document.getElementById(cityName).style.display = "block";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+//customer service part
+function btnClicked(){
+	document.location.href="index1.html";
+}
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+//background attachment
+$( window ).scroll( function(){
+  var ypos = $( window ).scrollTop(); //pixels the site is scrolled down
+  var visible = $( window ).height(); //visible pixels
+  const img_height = 1080; //replace with height of your image
+  var max_scroll = img_height - visible; //number of pixels of the image not visible at bottom
+//change position of background-image as long as there is something not visible at the bottom  
+if ( max_scroll > ypos) {
+     $('body').css('background-position', "center -" + ypos + "px");
+  } else {
+    $('body').css('background-position', "center -" + max_scroll + "px");
+  }
+});
